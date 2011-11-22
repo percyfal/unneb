49883eeca93117e2d2edfc2c91ab3683764ee890
Per Unneberg <per.unneberg@scilifelab.se>
2011-11-22 11:31:07 +0100

    Fix sbatch wrapper

--- ngs/ngs/paver/parallel/sbatch.py
+++ ngs/ngs/paver/parallel/sbatch.py
@@ -1,7 +1,9 @@
+"""
+sbatch wrapper
+"""
 import os
 from mako.template import Template
 from paver.easy import *
-
 
 SBATCH_TEMPLATE = Template('''\
 #!/bin/bash -l
@@ -29,7 +31,8 @@ ${command_str}
 ${footer}
 ''')
 
-options.sbatch.template = SBATCH_TEMPLATE
+options.sbatch_config = Bunch(
+    template = SBATCH_TEMPLATE
     # kw = Bunch(
     #     project_id = '',
     #     time = "50:00:00",
@@ -44,19 +47,27 @@ options.sbatch.template = SBATCH_TEMPLATE
     #     footer = '',
     #     command_str = '',
     #     )
+    )
 
-
+# TODO: should check that kw has all parameters
 def sbatch(command):
     """Wraps an external command in a sbatch template script"""
-    kw = options.sbatch.kw
-    kw.command_str = command
+    options.order("sbatch", "sbatch_config")
+    kw = options.get("kw", None)
+    if kw is None:
+        print >> sys.stderr, "sbatch keyword dictionary not defined: please set options for project_id, time etc"
+        sys.exit()
+    kw["command_str"] = command
+    if kw["jobname"] == "":
+        kw["jobname"] = "paver_sbatch"
+    kw["workdir"] = os.path.abspath(kw["workdir"])
     if options.dry_run:
-        print >> sys.stderr, options.sbatch.template.render(**kw)
+        print >> sys.stderr, options.sbatch_config.template.render(**kw)
     else:
         outfile = options.sbatch.kw.get("outfile", None)
         if not outfile is None:
             fp = open(outfile, "w")
-            fp.write(options.sbatch.template.render(**kw))
+            fp.write(options.sbatch_config.template.render(**kw))
             fp.close()
 
 ## Set exec_fn to sbatch
