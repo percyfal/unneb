6a4046232bd98b9dc10d213ed7bd10c4ff086dd1
Per Unneberg <per.unneberg@scilifelab.se>
2011-12-20 08:25:16 +0100

    Add illumina platform functions

---
+++ ngs/ngs/paver/platform/illumina.py
@@ -0,0 +1,28 @@
+"""
+Illumina related code.
+"""
+import os
+import glob
+import re
+from paver.easy import *
+from ngs.paver import run_cmd
+
+def organize_illumina_input(ext="_fastq.txt"):
+    """Simple grouping of illumina read files.
+
+    NOTE: currently assumes paired end reads.
+    """
+    bcdirs = glob.glob(os.path.join(options.dirs.data, "*", "*_barcode"))
+    fastafiles = []
+    for d in bcdirs:
+        fastafiles = fastafiles + glob.glob(os.path.join(d, "*" + ext))
+    samplenames = set([re.sub("_[1-2]" + ext, "", x) for x in fastafiles])
+    samples = []
+    for sn in samplenames:
+        f1 = sn + "_1" + ext
+        f2 = sn + "_2" + ext
+        if os.path.exists(f1) and os.path.exists(f2):
+            samples.append((os.path.basename(sn), f1, f2))
+        else:
+            print "WARNING: %s or %s missing!" % (f1, f2)
+    return samples
--- scilife/scilife/templates/__init__.py
+++ scilife/scilife/templates/__init__.py
@@ -3,5 +3,25 @@ templates config
 """
 
 import os
+from mako.template import Template
 
 TEMPLATE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+SBATCH_HEADER_TEMPLATE = Template('''\
+#!/bin/bash -l
+TMPDIR=/scratch/$SLURM_JOB_ID
+
+#SBATCH -A ${project_id}
+#SBATCH -t ${time}
+#SBATCH -o ${jobname}.stdout
+#SBATCH -e ${jobname}.stderr
+#SBATCH -J ${jobname}
+#SBATCH -D ${workdir}
+#SBATCH -p ${partition}
+#SBATCH -n ${cores}
+#SBATCH --mail-type=${mail_type}
+#SBATCH --mail-user=${mail_user}
+
+${header}
+''')
+
