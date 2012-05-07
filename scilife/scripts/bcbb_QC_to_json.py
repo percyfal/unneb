2505453f0d18053ae720fb094529e5d66d00cfb5
Per Unneberg <per.unneberg@scilifelab.se>
2012-05-07 18:02:10 +0200

    Add metrics parsers

--- scilife/scripts/bcbb_QC_to_json.py
+++ scilife/scripts/bcbb_QC_to_json.py
@@ -18,6 +18,8 @@ import sys
 import re
 import yaml
 import xml.parsers.expat
+import couchdb
+import hashlib
 from optparse import OptionParser
 
 import glob
@@ -53,7 +55,6 @@ class ExtendedPicardMetricsParser(PicardMetricsParser):
         analysis = None
         while 1:
             line = in_handle.readline()
-            print line
             if line.startswith("# net.sf.picard.analysis") or line.startswith("# net.sf.picard.sam"):
                 break
         return line.rstrip("\n")
@@ -77,7 +78,7 @@ class ExtendedPicardMetricsParser(PicardMetricsParser):
         command = self._get_command(in_handle)
         header = self._read_off_header(in_handle)
         d = dict([[x, []] for x in header])
-        res = dict(FIRST_OF_PAIR = d, SECOND_OF_PAIR = d, PAIR = d)
+        res = dict(command=command, FIRST_OF_PAIR = d, SECOND_OF_PAIR = d, PAIR = d)
         while 1:
             info = in_handle.readline().rstrip("\n").split("\t")
             category = info[0]
@@ -88,18 +89,20 @@ class ExtendedPicardMetricsParser(PicardMetricsParser):
         return res
 
     def _parse_dup_metrics(self, in_handle):
+        command = self._get_command(in_handle)
         header = self._read_off_header(in_handle)
         info = in_handle.readline().rstrip("\n").split("\t")
         vals = self._read_vals_of_interest(header, header, info)
         histvals = self._read_histogram(in_handle)
-        return dict(metrics = vals, hist = histvals)
+        return dict(command=command, metrics = vals, hist = histvals)
 
     def _parse_insert_metrics(self, in_handle):
+        command = self._get_command(in_handle)
         header = self._read_off_header(in_handle)
         info = in_handle.readline().rstrip("\n").split("\t")
         vals = self._read_vals_of_interest(header, header, info)
         histvals = self._read_histogram(in_handle)
-        return dict(metrics = vals, hist = histvals)
+        return dict(command=command, metrics = vals, hist = histvals)
 
     def _read_histogram(self, in_handle):
         labels = self._read_to_histogram(in_handle)
@@ -120,35 +123,33 @@ class ExtendedPicardMetricsParser(PicardMetricsParser):
                 break
         return in_handle.readline().rstrip("\n").split("\t")
 
-class RunInfoParser():
+class RunInfoParser(dict):
     """RunInfo parser"""
     def __init__(self):
-        self.data = dict()
-        self.element = None
+        self._element = None
 
     def parse(self, fp):
         self._parse_RunInfo(fp)
 
     def _start_element(self, name, attrs):
-        self.element=name
+        self._element=name
         if name == "Run":
-            self.data["Id"] = attrs["Id"]
-            self.data["Number"] = attrs["Number"]
+            self["Id"] = attrs["Id"]
+            self["Number"] = attrs["Number"]
         elif name == "FlowcellLayout":
-            self.data["FlowcellLayout"] = attrs
+            self["FlowcellLayout"] = attrs
         elif name == "Read":
-            self.data["Reads"].append(attrs)
+            self["Reads"].append(attrs)
             
     def _end_element(self, name):
-        self.element=None
+        self._element=None
 
     def _char_data(self, data):
         want_elements = ["Flowcell", "Instrument", "Date"]
-        if self.element in want_elements:
-            self.data[self.element] = data
-        if self.element == "Reads":
-            self.data["Reads"] = []
-            
+        if self._element in want_elements:
+            self[self._element] = data
+        if self._element == "Reads":
+            self["Reads"] = []
 
     def _parse_RunInfo(self, fp):
         p = xml.parsers.expat.ParserCreate()
@@ -157,91 +158,143 @@ class RunInfoParser():
         p.CharacterDataHandler = self._char_data
         p.ParseFile(fp)
     
-    def to_json(self):
-        return json.dumps(self.data)
 
-class QCLane():
+class QCLane(dict):
     """Lane level class for holding qc data"""
     def __init__(self, flowcell, lane ):
-        pass
+        self.lane = lane
 
-class QCSample():
+class QCSample(dict):
     """Sample-level class for holding qc metrics data"""
 
-    _metrics = ["align_metrics",
-                "dup_metrics","hs_metrics","insert_metrics",
-                "eval_metrics","fastqc","fastq_scr"]
+    _metrics = ["picard_metrics","fastqc","fastq_scr"]
 
     def __init__(self, flowcell, date, lane, barcode_name, barcode_id, sample_prj, sequence=None, barcode_type=None, genomes_filter_out=None):
-        self.sample = {}
-        self.sample["flowcell"] = flowcell
-        self.sample["date"] = date
-        self.sample["lane"] = lane
-        self.sample["barcode_name"] = barcode_name
-        self.sample["barcode_id"] = barcode_id
-        self.sample["sample_prj"] = sample_prj
-        self.sample["sequence"] = sequence
-        self.sample["barcode_type"] = barcode_type
-        self.sample["genomes_filter_out"] = genomes_filter_out
-        self.metrics = {}
-        for m in _metrics:
-            self.metrics[m] = {}
-    
-    def to_json(self):
-        return json.dumps({'sample':self.sample, 'metrics':self.metrics})
+        self["sample"] = {}
+        self["sample"]["flowcell"] = flowcell
+        self["sample"]["date"] = date
+        self["sample"]["lane"] = lane
+        self["sample"]["barcode_name"] = barcode_name
+        self["sample"]["barcode_id"] = barcode_id
+        self["sample"]["sample_prj"] = sample_prj
+        self["sample"]["sequence"] = sequence
+        self["sample"]["barcode_type"] = barcode_type
+        self["sample"]["genomes_filter_out"] = genomes_filter_out
+        self["metrics"] = {}
+        self.picard_files = []
+        for m in self._metrics:
+            self["metrics"][m] = {}
 
-class QCFlowcell():
+    def get_name(self, nophix=False):
+        if nophix:
+            s = "%s_%s_%s_nophix_%s" % (self["sample"]["lane"], self["sample"]["date"], self["sample"]["flowcell"], self["sample"]["barcode_id"])
+        else:
+            s = "%s_%s_%s_%s"  % (self["sample"]["lane"], self["sample"]["date"], self["sample"]["flowcell"], self["sample"]["barcode_id"])
+        return s
+
+class QCFlowcell(dict):
     """Flowcell level class for holding qc data"""
     _metrics = ["RunInfo", "run_info_yaml"]
 
     def __init__(self, flowcell_dir, archive_dir):
         self.flowcell_dir = flowcell_dir
         self.archive_dir = archive_dir
-        self.samples = dict()
-        self.lanes = dict()
-        self.metrics = dict()
+        self["_id"] = None
+        self["name"] = None
+        self["sample"] = dict()
+        self["lane"] = dict()
+        self["metrics"] = dict()
         for m in self._metrics:
-            self.metrics[m] = None
+            self["metrics"][m] = None
 
     def parseRunInfo(self, fn="RunInfo.xml"):
         fp = open(os.path.join(self.archive_dir, fn))
         parser = RunInfoParser()
         parser.parse(fp)
         fp.close()
-        self.metrics["RunInfo"] = parser.to_json()
+        self["metrics"]["RunInfo"] = parser
+        self["_id"] = self.get_db_id()
+        self["name"] = self.get_id()
+
 
     def parse_run_info_yaml(self, fn="run_info.yaml"):
         fp = open(os.path.join(self.archive_dir, fn))
         runinfo = yaml.load(fp)
         fp.close()
         for info in runinfo:
-            if not self.lanes.has_key(info["lane"]):
+            if not self["lane"].has_key(info["lane"]):
                 lane = QCLane(None, info["lane"])
-        self.metrics["run_info_yaml"] = json.dumps(runinfo)
-        
+                self["lane"][info["lane"]] = lane
+            for mp in info["multiplex"]:
+                sample = QCSample(self.get_flowcell(), self.get_date(), info["lane"], mp["name"], mp["barcode_id"], mp["sample_prj"], mp["sequence"], mp["barcode_type"], mp["genomes_filter_out"])
+                bc_index = "%s_%s" % (info["lane"], mp["barcode_id"])
+                self["sample"][bc_index] = sample
+        self["metrics"]["run_info_yaml"] = runinfo
+
+    def get_flowcell(self):
+        return self["metrics"]["RunInfo"]["Flowcell"]
+    def get_date(self):
+        return self["metrics"]["RunInfo"]["Date"]
+    def get_id(self):
+        return self["metrics"]["RunInfo"]["Id"]
+    def get_db_id(self):
+        return hashlib.md5(self.get_id()).hexdigest()
+    def get_run_id(self, run_prefix=0):
+        return self["metrics"]["RunInfo"]["Id"].replace("%s_%s%s_" % (self["metrics"]["RunInfo"]["Instrument"], run_prefix, self["metrics"]["RunInfo"]["Number"]), "")
+
     def to_json(self):
-        return self.metrics
+        samples = [self["sample"][s] for s in self["sample"]]
+        return json.dumps({'metrics':self["metrics"], 'samples':samples})
 
     def read_picard_metrics(self):
         picard_parser = ExtendedPicardMetricsParser()
-        files = self.get_metrics(self.flowcell_dir)
+        files = self._get_metrics(self.flowcell_dir)
+        # Group files to samples
         for fn in files:
+            fn_tgt = os.path.basename(fn).replace("_nophix_", "_")
             re_str = r'(\d+)_(\d+)_([A-Z0-9a-z]+XX)_?([a-zA-Z0-9]+)?-'
-            m = re.search(re_str, fn)
+            m = re.search(re_str, fn_tgt)
             (lane, date, flowcell, bc) = m.groups()
-            sample = "_".join([lane, str(bc)])
-            metrics = picard_parser.extract_metrics([mf])
-            print metrics
+            bc_index = "%s_%s" % (lane, bc)
+            if self["sample"].has_key(bc_index):
+                print >> sys.stderr, "reading metrics %s for sample %s" % (fn, bc_index)
+            else:
+                print >> sys.stderr, "WARNING: no sample %s for metrics %s" % (bc_index, fn)
+            self["sample"][bc_index].picard_files.append(fn)
+        for s in self["sample"]:
+            metrics = picard_parser.extract_metrics(self["sample"][s].picard_files)
+            self["sample"][s]["metrics"]["picard_metrics"] = metrics
+
 
     def _get_metrics(self, indir, re_str='.*.(align|hs|insert|dup)_metrics'):
         matches = []
         for root, dirnames, filenames in os.walk(indir):
             for fn in filenames:
-                fn = os.path.basename(mf).replace("_nophix_", "_")
-                if re.match('', fn):
+                if re.match(re_str, fn):
                     matches.append(os.path.join(root, fn))
         return matches
 
+    def read_fastqc_metrics(self):
+        for s in self["sample"]:
+            d = glob.glob(os.path.join(self.flowcell_dir, "fastqc", "%s_%s_*_%s*" % (self["sample"][s]["sample"]["lane"], self.get_run_id(), self["sample"][s]["sample"]["barcode_id"])))
+            fastqc_dir=d[0]
+            fqparser = ExtendedFastQCParser(fastqc_dir)
+            #graphs = fqparser.get_fastqc_graphs()
+            stats = fqparser.get_fastqc_summary()
+            self["sample"][s]["metrics"]["fastqc"] = {'stats':stats}
+            # print stats
+            # print overrep
+            # print graphs
+        
+class ExtendedFastQCParser(FastQCParser):
+    def __init__(self, base_dir):
+        FastQCParser.__init__(self, base_dir)
+
+    def get_fastqc_summary(self):
+        for line in self._fastqc_data_section("Per base sequence quality"):
+            if line.startswith("#"):
+                print line.split("\t")
+    
 
 def main(config_file, fc_dir, run_info_yaml=None):
     config = load_config(config_file)
@@ -258,28 +311,18 @@ def run_main(fc_dir, qcdb_store_dir):
     qc_obj = QCFlowcell(fc_dir, fc_dir)
     qc_obj.parseRunInfo()
     qc_obj.parse_run_info_yaml()
-    print qc_obj.to_json()
-    sys.exit()
-#     for mf in metrics_files:
-#         fn = os.path.basename(mf).replace("_nophix_", "_")
-#         print fn
-#         if fn.endswith("bc.metrics"):
-#             re_str = r'(\d+)_(\d+)_([A-Z0-9a-z]+XX).*'
-#             m = re.search(re_str, fn)
-#             (lane, date, flowcell) = m.groups()
-#             bc = None
-#         elif fn.endswith("filter.metrics"):
-#             pass
-# #        else:
-        
-    fastqc_dir = "/Users/peru/Org/ScilifeCore/delivery_data/projects/SOLiD_JPA/fastqc/GMS_D1_F3.single_fastqc"
-    fastqc_file = "/Users/peru/Org/ScilifeCore/delivery_data/projects/SOLiD_JPA/fastqc/GMS_D1_F3.single_fastqc/fastqc_data.txt"
-    fqparser = FastQCParser(fastqc_dir)
-    graphs = fqparser.get_fastqc_graphs()
-    stats, overrep = fqparser.get_fastqc_summary()
-    print stats
-    print overrep
-    print graphs
+    qc_obj.read_picard_metrics()
+    qc_obj.read_fastqc_metrics()
+    couch = couchdb.Server("http://maggie.scilifelab.se:5984")
+    db = couch['qc']
+    obj = db.get(qc_obj.get_db_id())
+    if obj is None:
+        db.save(qc_obj)
+    else:
+        pass
+    #qc_obj["_rev"] = obj["_rev"]
+    #    db.save(qc_obj)
+    #print qc_obj
 
 if __name__ == "__main__":
     usage = """
